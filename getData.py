6942d3e71386bd1b7ac1bdbbb33535bc6bc8f4e2
Chnadra Prakash <45210459+cpsln@users.noreply.github.com>
2019-07-27 03:01:16 -0700

    get User data which are login

---
+++ getData.py
@@ -0,0 +1,40 @@
+
+from flask import request,session,jsonify
+from db_config import demo
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/PatientData', methods=['POST'])
+def patientdata():
+        if request.method =='POST':
+            data=demo.find()
+            payload=request.get_json()
+            print(payload)
+            for i in data:
+
+                    if payload['userId'] == str(i['_id']):
+
+                        i['_id']=str(i['_id'])
+                        response = jsonify([i])
+                        response.headers.add('Access-Control-Allow-Origin', '*')
+                        response.headers['Access-Control-Allow-Origin'] = '*'
+                        print(response)
+                        print(response.headers)
+                        return response
+                    else:
+                        pass
+
+            response = jsonify({'status':'no data found'})
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            print(response)
+            print(response.headers)
+            return response
+            
+
+if __name__ == '__main__':
+
+    app.secret_key = 'some secret key'
+    app.debug = True
+    app.run(port=5001)
