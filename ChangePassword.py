806fa90ea2b65ddcd313c602db140dffef9a5a75
Chnadra Prakash <45210459+cpsln@users.noreply.github.com>
2019-07-27 03:07:13 -0700

    User change password

---
+++ ChangePassword.py
@@ -0,0 +1,55 @@
+from flask import request,session,jsonify
+from flask import Flask
+from db_config import demo
+
+app=Flask(__name__)
+
+
+
+@app.route('/changePassword',methods = ['POST'])
+def changePassword():
+
+        if request.method == 'POST':
+
+            payload=request.get_json()
+            print(payload)
+
+            for dId in doctor.find():
+
+                if payload['UserId'] == str(dId['_id']):
+                    if payload['Passwordnew'] == payload['Passwordconf']:
+                        doctor.update_one({
+                                    '_id':dId['_id']
+                            },{
+                                '$set':{
+                                'password':payload['Passwordnew']
+                                }
+                            }
+                        )
+                        response = jsonify({'status':'save password'})
+                        response.headers.add('Access-Control-Allow-Origin', '*')
+                        response.headers['Access-Control-Allow-Origin'] = '*'
+                        print(response)
+                        print(response.headers)
+                        return response
+                    else:
+                        response = jsonify({'status':'password not match'})
+                        response.headers.add('Access-Control-Allow-Origin', '*')
+                        response.headers['Access-Control-Allow-Origin'] = '*'
+                        print(response)
+                        print(response.headers)
+                        return response,201
+
+            response = jsonify({'status':'Not save password'})
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            print(response)
+            print(response.headers)
+            return response,205
+
+
+if __name__ == '__main__':
+
+    app.secret_key = 'some secret key'
+    app.debug = True
+    app.run(port=5001)
