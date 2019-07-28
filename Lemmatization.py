ab354a31d6bd177286b7a9d8ee67feb263e0da02
Chnadra Prakash <45210459+cpsln@users.noreply.github.com>
2019-07-27 23:52:25 -0700

    Stemming and Lemmatization

---
+++ Lemmatization.py
@@ -0,0 +1,17 @@
+from nltk.stem import WordNetLemmatizer 
+from nltk.stem import PorterStemmer 
+stemmer = PorterStemmer() 
+lemmatizer = WordNetLemmatizer() 
+print(stemmer.stem('stones')) 
+print(stemmer.stem('speaking')) 
+print(stemmer.stem('bedroom')) 
+print(stemmer.stem('jokes')) 
+print(stemmer.stem('lisa')) 
+print(stemmer.stem('purple')) 
+print('----------------------') 
+print(lemmatizer.lemmatize('stones')) 
+print(lemmatizer.lemmatize('speaking'))
+print(lemmatizer.lemmatize('bedroom'))
+print(lemmatizer.lemmatize('jokes'))
+print(lemmatizer.lemmatize('lisa'))
+print(lemmatizer.lemmatize('purple'))
