60f78618f1690bdde5f9f908595fadc975c8e43d
Chnadra Prakash <45210459+cpsln@users.noreply.github.com>
2019-07-28 05:06:45 -0700

    show all user data to admin

---
+++ allUserData.py
@@ -0,0 +1,40 @@
+
+from flask import request,session,jsonify
+from db_config import demo
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/allUserData', methods=['POST'])
+def allUser():
+        if request.method =='POST':
+            data=demo.find()
+            payload=request.get_json()
+            print(payload)
+            alldata=[]
+            for i in data:
+
+
+                        i['_id']=str(i['_id'])
+                        response = jsonify(alldata)
+                        response.headers.add('Access-Control-Allow-Origin', '*')
+                        response.headers['Access-Control-Allow-Origin'] = '*'
+                        print(response)
+                        print(response.headers)
+                        return response
+                    else:
+                        pass
+
+            response = jsonify({'status':'no data found'})
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            print(response)
+            print(response.headers)
+            return response
+            
+
+if __name__ == '__main__':
+
+    app.secret_key = 'some secret key'
+    app.debug = True
+    app.run(port=5001)
